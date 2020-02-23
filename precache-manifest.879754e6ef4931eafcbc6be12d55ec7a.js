0394f179c38a3d6d688d0963ef8ff5de52faa180
iamsomraj <iamsomraj@gmail.com>
2020-02-23 17:14:32 +0530

    Updates

---
+++ precache-manifest.879754e6ef4931eafcbc6be12d55ec7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a213c0e2552c37fecfa6ff10c26b4b4",
+    "url": "/React-Movie-Database/index.html"
+  },
+  {
+    "revision": "d96b1e9dfa23addad5a0",
+    "url": "/React-Movie-Database/static/css/main.0e0797f5.chunk.css"
+  },
+  {
+    "revision": "7994ff7da11d620edb2e",
+    "url": "/React-Movie-Database/static/js/2.4f33c17a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Movie-Database/static/js/2.4f33c17a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d96b1e9dfa23addad5a0",
+    "url": "/React-Movie-Database/static/js/main.c83a186c.chunk.js"
+  },
+  {
+    "revision": "1fc74fbbf447d2db18d5",
+    "url": "/React-Movie-Database/static/js/runtime-main.dc5cb583.js"
+  }
+]);
